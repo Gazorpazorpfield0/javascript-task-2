0e28c1e2be27d2c8d942125e43f18a209cfdec3c
ae.ksenofontov <ae.ksenofontov@tensor.ru>
2020-03-13 09:35:09 +0500

    '

--- phone-book.js
+++ phone-book.js
@@ -8,39 +8,39 @@ function phoneExistance(phone) {
     }
     for (let i = 0; i <= phoneBook.length; i++) {
         if (phoneBook[i].phone === phone) {
-            return true; 
+            return true;
         } else {
             i++
         }
-    return false;    
-    }
-}
-
-function Add(phone, name, mail) {
-    // проверяем валидность введенных данных
-    // пропускает неповторя
-    let phoneRegExp = /^5{3}[0-9]{3}[0-9]{2}[0-9]{2}$/ 
-    if ((phoneRegExp.test(phone)) && (!phoneExistance(phone))) {
-        phoneBook.push({phone: phone, name: name, mail: mail});
-        return true;
-    } else {
-        return false;
+    return false;
     }
 };
 
+function Add(phone, name, mail) {
+    // проверяем валидность введенных данных
+    // переписать с более простым вариантом (цифры могут быть в свободном порядке, беез повторений:
+    // главное, чтобы отфильтровывало по кол-ву символов
+    let phoneRegExp = /^5{3}[0-9]{3}[0-9]{2}[0-9]{2}$/;
+    if ((phoneRegExp.test(phone)) && (!phoneExistance(phone))) {
+        phoneBook.push({phone: phone, name: name, mail: mail});
+        return true;
+    }
+    return false;
+};
+
 function Update(phone, name, mail) {
-    if (mail === undefined) mail === '';
-    if (name === undefined) return false;
+    if (typeof mail === 'undefined') mail === '';
+    if (typeof name === 'undefined') return false;
     for (let i = 0; i <= phoneBook.length; i++) {
         if (phoneBook[i].phone === phone) {
             phoneBook[i].name = name;
             phoneBook[i].mail = mail;
-            return true; 
+            return true;
         } else {
             i++
         }
-    return false;    
     }
+    return false;
 };
 
 phoneBook.Add = Add;
