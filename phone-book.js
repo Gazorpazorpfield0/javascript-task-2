6a8e7deab26add1552ed17c2600b71c5bf58a051
Gazorpazorpfield0 <toxa.zippa@gmail.com>
2020-03-19 21:53:34 +0500

    Mocha update, package-lock.json update and alfa version of phone-book.js

--- phone-book.js
+++ phone-book.js
@@ -8,11 +8,11 @@ function phoneExistance(phone) {
     }
     for (let i = 0; i < phoneBook.length;) {
         if (phoneBook[i].phone === phone) {
-            return true; 
-        } else {
-            i++
-        }   
+            return true;
+        }
+        i++;
     }
+
     return false;
 }
 
@@ -22,53 +22,81 @@ function phoneBeauty(phone) {
 
 function Add(phone, name, mail) {
     // проверяем валидность введенных данных
-    // пропускает неповторя
     let phoneRegExp = /^[0-9]{10}$/;
     if ((phoneRegExp.test(phone)) && (!phoneExistance(phone))) {
-        phoneBook.push({phone: phone, name: name, mail: mail});
+        phoneBook.push({ phone: phone, name: name, mail: mail });
+
         return true;
     }
+
     return false;
-};
+}
 
 function Update(phone, name, mail) {
     if (phoneExistance(phone)) {
-        if (typeof mail === 'undefined') mail = '';
-        if (typeof name === 'undefined') return false;
+        if (typeof mail === 'undefined') {
+            mail = '';
+        }
+        if (typeof name === 'undefined') {
+            return false;
+        }
         for (let i = 0; i < phoneBook.length;) {
             if (phoneBook[i].phone === phone) {
                 phoneBook[i].name = name;
                 phoneBook[i].mail = mail;
-                return true; 
-            } else {
-                i++;
+
+                return true;
             }
+            i++;
         }
     }
+
     return false;
-};
+}
 
 function Find(str) {
+    if (str === '') {
+        return [];
+    }
     if (str === '*') {
         let phoneBookCopy = phoneBook;
-        let sortedPhoneBook = phoneBook.sort((a, b) => a.name > b.name ? 1 : -1);
-        let outputAllPhoneBook = sortArray.map((item) => `${item.name}, ${phoneBeauty(item.phone)}, ${item.mail} `);
-        return console.log(outputAllPhoneBook);
+        let sortedPhoneBook = phoneBookCopy.sort((a, b) => a.name > b.name ? 1 : -1);
+        let outputAllPhoneBook = sortedPhoneBook.map((item) => '\n' + `${item.name}, ${phoneBeauty(item.phone)}, ${item.mail} `);
+
+        return outputAllPhoneBook;
     }
     let findArray = phoneBook.filter((elem, index) => String(Object.values(phoneBook[index])).indexOf(str) !== -1);
     let sortArray = findArray.sort((a, b) => a.name > b.name ? 1 : -1);
-    let result = sortArray.map((item) => `${item.name}, ${phoneBeauty(item.phone)}, ${item.mail} `);
-    console.log(result);
-};
+    let result = sortArray.map((item) => '\n' + `${item.name}, ${phoneBeauty(item.phone)}, ${item.mail} `);
+
+    return result;
+}
+
+function findAndRemove(str) {
+    let elemsForRemove = phoneBook.find(str);
+    let beautyPhoneBook = phoneBook.find('*');
+    let counterOfRemoved = 0;
+
+    for (let i = elemsForRemove.length; i > 0; i--) {
+        for (let j = 0; j <= beautyPhoneBook.length; j++) {
+            if (elemsForRemove[i] === beautyPhoneBook[j]) {
+                phoneBook.splice(phoneBook[j], 1);
+                counterOfRemoved++;
+            }
+        }
+    }
+
+    return counterOfRemoved;
+}
 
 phoneBook.add = Add;
 phoneBook.update = Update;
 phoneBook.find = Find;
+phoneBook.findAndRemove = findAndRemove;
 
 phoneBook.add('5557772211', 'Валера', 'valera@gmail.com');
 phoneBook.add('5557772212', 'Борис', 'borya.zippa@gmail.com');
 phoneBook.add('5557772213', 'Антон', 'toxa.zippa@gmail.com');
-// phoneBook.update('5557772211', 'Boris');
-phoneBook.find('*');
-
-// console.log(phoneBook.length);
+phoneBook.add('3337572214', 'Евгений', 'evg@gmail.com');
+phoneBook.update('5557772211', 'Boris');
+phoneBook.findAndRemove('3337572214');
